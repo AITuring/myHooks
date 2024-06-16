4711d558f1676141ac266834f35016fcb28b4ad0
AITuring <shspku365@gmail.com>
2024-06-16 15:24:15 +0800

    perf: useContdown 新增暂停,重置,性能优化,减少重复渲染

--- src/HookTest.tsx
+++ src/HookTest.tsx
@@ -1,12 +1,14 @@
 import { useCountDown } from "./hooks";
 
 const HookTest: React.FC = () => {
-  const { time, start } = useCountDown(10);
+  const { time, start, pause, reset } = useCountDown(100);
   return (
     <div>
     <h1>useCountDown</h1>
       <h2>{time}</h2>
       <button onClick={() => start()}>开始计时</button>
+      <button onClick={() => pause()}>停止计时</button>
+      <button onClick={() => reset()}>重置计时</button>
     </div>
   );
 };
--- src/hooks/useCountDown/index.ts
+++ src/hooks/useCountDown/index.ts
@@ -1,40 +1,65 @@
-import {useState, useEffect, useRef} from "react";
+import {useState, useEffect, useRef, useCallback} from "react";
 
 interface CountDown {
     time: number,
-    start: () => void
+    start: () => void,
+    pause: () => void,
+    reset: () => void,
 }
 
 const useCountDown = (maxDuration: number):CountDown => {
     const [time, setTime] = useState<number>(maxDuration);
+    const [isActive, setActive] = useState<boolean>(false);
     const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+    // 清除定时器
+    const clearTimer = useCallback(() => {
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+        }
+        intervalRef.current = null;
+    }, [])
 
     useEffect(() => {
         return () => {
-            if (intervalRef.current) {
-                clearInterval(intervalRef.current)
-            }
+            clearTimer();
         }
-    }, [])
+    }, [clearTimer])
 
-    const start = () => {
-        if (intervalRef.current) {
-            clearInterval(intervalRef.current)
-        }
+    const start = useCallback(() => {
+        if (isActive) return;
 
-        setTime(maxDuration)
+        setActive(true);
+
         intervalRef.current = setInterval(() => {
             setTime(prevTime => {
-                if (prevTime === 1 && intervalRef.current) {
-                    clearInterval(intervalRef.current)
+                if (prevTime === 1) {
+                    clearTimer();
+                    setActive(false);
+                    return 0;
                 }
-
                 return prevTime - 1
-            })
-        }, 1000)
-    }
+            });
+        }, 1000);
+    } ,[isActive, clearTimer])
 
-    return {time, start}
+    const pause = useCallback(() => {
+        setActive(false);
+        clearTimer();
+    }, [clearTimer])
+
+    const reset = useCallback(() => {
+        setActive(false);
+        clearTimer();
+        setTime(maxDuration)
+    }, [clearTimer, maxDuration])
+
+    return {
+        time,
+        start,
+        pause,
+        reset
+    }
 }
 
 export default useCountDown;
