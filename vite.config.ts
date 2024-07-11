9c068e296e5e206f725f6a804d2b1b97fc0b95d1
AITuring <shspku365@gmail.com>
2024-07-11 09:42:43 +0800

    perf: 配置vite,开启分包和模块压缩

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import viteCompression from "vite-plugin-compression";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test:{
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-    coverage: {
-      provider: 'v8', // or 'istanbul'
-      reporter: ['html']
+    plugins: [
+        react(),
+        viteCompression({
+            verbose: true, // 是否在控制台中输出压缩结果
+            disable: false,
+            threshold: 10240, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
+            algorithm: "gzip", // 压缩算法，可选['gzip'，' brotliccompress '，'deflate '，'deflateRaw']
+            ext: ".gz",
+            deleteOriginFile: true, // 源文件压缩后是否删除(我为了看压缩后的效果，先选择了true)
+        }),
+        visualizer({ open: true }),
+    ],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: ["./src/setupTests.ts"],
+        coverage: {
+            provider: "v8", // or 'istanbul'
+            reporter: ["html"],
+        },
     },
-  }
-})
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes("node_modules")) {
+                        return "vendor";
+                    } else {
+                        return "common";
+                    }
+                },
+            },
+        },
+    },
+});
