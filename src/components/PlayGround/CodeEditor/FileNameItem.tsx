28f0d81a31fb146e8425ba097cb5e4c40f5fc964
AITuring <shspku365@gmail.com>
2024-09-10 11:41:22 +0800

    feat: 实现创建文件与删除文件

--- src/components/PlayGround/CodeEditor/FileNameItem.tsx
+++ src/components/PlayGround/CodeEditor/FileNameItem.tsx
@@ -1,29 +1,100 @@
 import classNames from "classnames";
-import style from './fileName.module.css';
-import React from "react";
+import style from "./fileName.module.css";
+import { useState, useRef, useEffect, FC, MouseEventHandler } from "react";
 
 export interface FileNameItemProps {
-    value: string,
-    actived: boolean,
-    onClick: () => void
+    value: string;
+    actived: boolean;
+    creating: boolean;
+    onEditComplete: (name: string) => void;
+    onClick: () => void;
+    onRemove: MouseEventHandler;
+    readonly: boolean;
 }
 
-export const FileNameItem: React.FC<FileNameItemProps> = props => {
+export const FileNameItem: FC<FileNameItemProps> = (props) => {
     const {
         value,
         actived = false,
         onClick,
+        onEditComplete,
+        creating,
+        onRemove,
+        readonly,
     } = props;
 
-    // const [name, setName] = useState<string>(value);
+    const [name, setName] = useState<string>(value);
+    const [editing, setEditing] = useState<boolean>(creating);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleDoubleClick = () => {
+        setEditing(true);
+        setTimeout(() => {
+            inputRef.current?.focus();
+        }, 0);
+    };
+
+    const handleBlur = () => {
+        setEditing(false);
+        onEditComplete(name);
+    };
+
+    useEffect(() => {
+        if (creating) {
+            inputRef?.current?.focus();
+        }
+    }, [creating]);
 
     return (
-        <
-            div
-            className={classNames(style.tabItem, actived ? style.actived : null)}
+        <div
+            className={classNames(
+                style.tabItem,
+                actived ? style.actived : null
+            )}
             onClick={onClick}
         >
-            <span>{value}</span>
+            {editing ? (
+                <input
+                    ref={inputRef}
+                    onBlur={handleBlur}
+                    className={style.tabItemInput}
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            ) : (
+                <>
+                    <span
+                        onDoubleClick={readonly ? () => {} : handleDoubleClick}
+                    >
+                        {value}
+                    </span>
+                    {readonly ? null : (
+                        <span
+                            style={{ marginLeft: 5, display: "flex" }}
+                            // TODO 点击应该加一个popover确认一下
+                            onClick={onRemove}
+                        >
+                            <svg width="12" height="12" viewBox="0 0 24 24">
+                                <line
+                                    stroke="#999"
+                                    x1="18"
+                                    y1="6"
+                                    x2="6"
+                                    y2="18"
+                                ></line>
+                                <line
+                                    stroke="#999"
+                                    x1="6"
+                                    y1="6"
+                                    x2="18"
+                                    y2="18"
+                                ></line>
+                            </svg>
+                        </span>
+                    )}
+                </>
+            )}
         </div>
-    )
-}
+    );
+};
--- src/components/PlayGround/CodeEditor/FileNameList.tsx
+++ src/components/PlayGround/CodeEditor/FileNameList.tsx
@@ -1,17 +1,40 @@
 import { useContext, useState, useEffect } from "react";
 import { PlayGroundContext } from "../PlayGroundContext";
 import { FileNameItem } from "./FileNameItem";
+import { ENTRY_FILE_NAME,IMPORT_MAP_FILE_NAME,APP_COMPONENT_FILE_NAME } from '../file';
 import style from './fileName.module.css';
 
 const FileNameList = () => {
-    const { files, selectedFileName, setSelectedFileName } =
+    const { files, selectedFileName, setSelectedFileName, updateFileName, addFile, removeFile } =
         useContext(PlayGroundContext);
 
     const [tabs, setTabs] = useState([""]);
+    const [creating, setCreating] = useState(false);
+
+    const readonlyFileNames = [ENTRY_FILE_NAME,IMPORT_MAP_FILE_NAME,APP_COMPONENT_FILE_NAME];
 
     useEffect(() => {
         setTabs(Object.keys(files));
     }, [files]);
+
+    const handleEditComplate = (name: string, prevName: string) => {
+        updateFileName(prevName, name);
+        setSelectedFileName(name);
+        setCreating(false);
+    }
+
+    const addTab = () => {
+        const newFileName = `Comp${Math.random().toString().slice(2, 8)}.tsx`;
+        addFile(newFileName);
+        setSelectedFileName(newFileName);
+        setCreating(true);
+    }
+
+    const handleRemove = (name: string) => {
+        removeFile(name);
+        setSelectedFileName(ENTRY_FILE_NAME);
+
+    };
 
     return (
         <div className={style.tabs}>
@@ -20,11 +43,19 @@ const FileNameList = () => {
                     <FileNameItem
                         key={item + index}
                         value={item}
+                        readonly={readonlyFileNames.includes(item)}
+                        creating={creating && index === tabs.length - 1}
                         actived={selectedFileName === item}
                         onClick={() => setSelectedFileName(item)}
+                        onEditComplete={(name: string) => handleEditComplate(name, item)}
+                        onRemove={(e) => {
+                            e.stopPropagation();
+                            handleRemove(item);
+                        }}
                     />
                 ))
             }
+            <div className={style.add} onClick={addTab}>+</div>
         </div>
     );
 };
