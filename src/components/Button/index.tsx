81ef3956349ff40207a70e23df824c604071a99d
AITuring <shspku365@gmail.com>
2024-09-18 17:42:40 +0800

    fix: 修复编译失败问题

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,13 @@ import React from "react";
 interface ButtonProps {
     onClick?: () => void;
     children: React.ReactNode;
+    ref?: React.Ref<HTMLButtonElement>;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, children, ref }) => {
     return (
         <button
+            ref={ref}
             onClick={onClick}
             className="block mb-2.5 px-4 py-2.5 text-base cursor-pointer rounded bg-[#002F21] text-gray-200 hover:bg-[#099d71] hover:text-[#F1F2F5] transition-colors duration-300"
         >
